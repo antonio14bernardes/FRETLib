379fb06d720aad484eeccd9dfcac53b116d29ecd
António Bernardes <antonio14bernardes@Antonios-MacBook-Air.local>
2024-09-16 14:58:05 +0200

    Setting up the structure.

---
+++ src/signal_analysis/hmm.rs
@@ -0,0 +1,13 @@
+/********** Hidden Markov Model (HMM) Module for FRET Analysis **********
+* This module is designed based on the methodology and algorithm described in the paper:
+*
+* "Analysis of Single-Molecule FRET Trajectories Using Hidden Markov Modeling"**
+*
+* Authors: Sean A. McKinney, Chirlmin Joo, and Taekjip Ha  
+* Department of Physics and Howard Hughes Medical Institute, University of Illinois at Urbana-Champaign  
+* Published in: *Biophysical Journal*, Volume 91, September 2006, Pages 1941–1951  
+* DOI: [10.1529/biophysj.106.082487](https://doi.org/10.1529/biophysj.106.082487)
+**********/
+
+
+mod hmm_tools;
