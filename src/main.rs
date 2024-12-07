d7239c78f39463f25c5316c735afd64f9118ad80
antonio14bernardes <antonio14bernardes@hotmail.com>
2024-12-07 21:37:23 +0000

    Adding main just to not have anoying problems

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use core::{f64, num};
 use std::cmp::min;
 use std::collections::HashSet;
 use std::iter::Filter;
+use std::thread;
 
 use fret_lib::optimization::amalgam_idea::{self, AmalgamIdea};
 use fret_lib::optimization::constraints::OptimizationConstraint;
@@ -15,7 +16,7 @@ use fret_lib::signal_analysis::hmm::optimization_tracker::{self, StateCollapseHa
 use fret_lib::signal_analysis::hmm::state::State;
 use fret_lib::signal_analysis::hmm::hmm_matrices::{StartMatrix, TransitionMatrix};
 use fret_lib::signal_analysis::hmm::viterbi::Viterbi;
-use fret_lib::signal_analysis::hmm::{baum_welch};
+use fret_lib::signal_analysis::hmm::{baum_welch, StateValueInitMethod};
 use fret_lib::signal_analysis::hmm::baum_welch::*;
 use fret_lib::trace_selection::filter::FilterSetup;
 use fret_lib::trace_selection::set_of_points::SetOfPoints;
@@ -57,7 +58,7 @@ fn main_hmm() {
     // Define real system to get simulated time sequence
     let real_state1 = State::new(0, 10.0, 1.0).unwrap();
     let real_state2 = State::new(1, 20.0, 2.0).unwrap();
-    let real_state3 = State::new(2, 23.0, 3.0).unwrap();
+    let real_state3 = State::new(2, 30.0, 3.0).unwrap();
 
     let real_states = vec![real_state1, real_state2, real_state3];
     let _num_states = real_states.len();
@@ -77,7 +78,7 @@ fn main_hmm() {
     let mut sequence_ids_set: Vec<Vec<usize>> = Vec::new();
     let num_sequences = 10;
     for _ in 0..num_sequences {
-        let (sequence_ids, sequence_values) = HMMInstance::gen_sequence(&real_states, &real_start_matrix, &real_transition_matrix, 1000);
+        let (sequence_ids, sequence_values) = HMMInstance::gen_sequence(&real_states, &real_start_matrix, &real_transition_matrix, 100);
         sequence_set.push(sequence_values);
         sequence_ids_set.push(sequence_ids);
     }
@@ -87,18 +88,42 @@ fn main_hmm() {
     let mut hmm = HMM::new();
 
     hmm.add_learner();
-    hmm.set_learner_type(LearnerType::AmalgamIdea).unwrap();
-    hmm.setup_learner(
-        LearnerSpecificSetup::AmalgamIdea { 
-            iter_memory: None, dependence_type: None, fitness_type: None, max_iterations: None
-        }).unwrap();
+    // hmm.set_learner_type(LearnerType::AmalgamIdea).unwrap();
+    // hmm.setup_learner(
+    //     LearnerSpecificSetup::AmalgamIdea { 
+    //         iter_memory: None, dependence_type: None, fitness_type: None, max_iterations: None
+    //     }).unwrap();
+
+    hmm.set_learner_type(LearnerType::BaumWelch).unwrap();
+    hmm.setup_learner(LearnerSpecificSetup::BaumWelch { termination_criterion: None }).unwrap();
+
     hmm.add_initializer().unwrap();
+    let mut init_method = InitializationMethods::new();
+    init_method.state_values_method = Some(StateValueInitMethod::StateHints { state_values: vec![10.0, 20.0, 30.0] });
+    hmm.set_initialization_method(init_method).unwrap();
 
     let input = HMMInput::Initializer { num_states: real_states.len(), sequence_set: sequence_set };
 
-    hmm.run(input).unwrap();
 
-    let analyzer = hmm.get_analyzer();
+
+
+
+
+    // hmm.run(input).unwrap();
+
+
+    // Launch a thread to run the HMM 
+    let handle = thread::spawn(move || {
+        match hmm.run(input) {
+            Ok(()) => Ok(hmm),
+            Err(e) => Err(e),
+        }
+        
+    });
+    let updated_hmm_result = handle.join().expect("Thread panicked");
+    let updated_hmm = updated_hmm_result.unwrap();
+
+    let analyzer = updated_hmm.get_analyzer();
     let opt_states = analyzer.get_states().unwrap();
     let opt_start_matrix = analyzer.get_start_matrix().unwrap();
     let opt_transition_matrix = analyzer.get_transition_matrix().unwrap();
